b3704b7d8a114f9d864abdb9d9d4da4c4df852ac
QGifford123 <cchi0106@gmail.com>
2026-06-11 10:42:27 -0700

    moved example auton into rightAuton()

--- src/autons.cpp
+++ src/autons.cpp
@@ -9,7 +9,20 @@ using namespace vex;
 
 void rightAuton() {
 
-  //Use functions to create an auton for the right side of the field
+  Controller.Screen.clearLine(3);
+  Controller.Screen.setCursor(3, 1);
+  Controller.Screen.print("start auton");
+
+  drive Chassis(3.25, 4.0/3, leftDrive, rightDrive, PORT1);
+  intake Intake(intakeMotors);
+
+  Chassis.driveDistance(24, 1, 1, 0, 0.1);
+  Intake.startIntake();
+  Chassis.turnAngle(90);
+  Chassis.turnAngle(-90);
+  wait(2, sec);
+  Intake.stopIntake();
+  Chassis.driveDistance(-24);
 
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ void pre_auton(void) {
 
 void autonomous(void) {
   
-  /*switch (getSelectedAuton()) {
+  switch (getSelectedAuton()) {
 
     case 0:
       rightAuton();
@@ -73,22 +73,7 @@ void autonomous(void) {
     default:
       Brain.Screen.print("No valid auton selected!");
       break;
-  }*/
- 
-  Controller.Screen.clearLine(3);
-  Controller.Screen.setCursor(3, 1);
-  Controller.Screen.print("start auton");
-
-  drive Chassis(3.25, 4.0/3, leftDrive, rightDrive, PORT1);
-  intake Intake(intakeMotors);
-
-  //Chassis.driveDistance(24, 1, 1, 0, 0.1);
-  Intake.startIntake();
-  //Chassis.turnAngle(90);
-  //Chassis.turnAngle(-90);
-  wait(2, sec);
-  Intake.stopIntake();
-  //Chassis.driveDistance(-24);
+  }
 
 
 }
